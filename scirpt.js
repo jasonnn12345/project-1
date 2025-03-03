e6c0ebc3ddca87670a71c8ddd38ef67d87d4eb30
AndreasZhann <andreaszhan4@gmail.com>
2025-03-03 13:17:40 +0700

    make a weater web with API

---
+++ scirpt.js
@@ -0,0 +1,24 @@
+const apiKey = "f38d63ee8a5dc07a3351bc19005ac49e";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+  } else {
+    let data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "ºC";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " Km/H";
+
+    document.querySelector(".weather").style.display = "block";
+  }
+}
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
